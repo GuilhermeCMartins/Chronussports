d7b24b50cdfe0a6c3850199027b3ecf00a39ca22
GuilhermeCMartins <guilhermecourbassier122@gmail.com>
2023-04-28 22:17:10 -0300

    feat: dropdowns and buttons

--- src/modules/club/components/CarouselClub/index.tsx
+++ src/modules/club/components/CarouselClub/index.tsx
@@ -1,70 +1,115 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Lazy } from 'swiper';
-import 'swiper/swiper-bundle.min.css';
+import React, { useState } from 'react';
+import SwiperCore, { Navigation, EffectCoverflow } from 'swiper';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import Image from 'next/image'
 
-import styles from './styles.module.css'
+import styles from './carousel.module.css'
 
+import 'swiper/swiper-bundle.css';
+import Dropdown from '../Dropdown/Dropdown';
 
-SwiperCore.use([Lazy]);
+type Images = {
+    items: {
+      key: number;
+      src: string;
+      alt: string;
+      caption: string;
+      width: number;
+      height: number;
+    }[];
+  };
 
-export default function CarouselClub(){
+SwiperCore.use([EffectCoverflow, Navigation]);
 
+const nacionalidade = [
+  { id: 1, label: 'Brasileiro(a)' },
+  { id: 2, label: 'Alemão' },
+  { id: 3, label: 'Espanhol' },
+  { id: 4, label: 'Italiano(a)' },
+];
+
+const modalidade = [
+  { id: 1, label: 'Futebol' },
+  { id: 2, label: 'Basquete' },
+  { id: 3, label: 'Vôlei' },
+  { id: 4, label: 'Futsal' },
+];
+
+const regiao = [
+  { id: 1, label: 'Norte' },
+  { id: 2, label: 'Nordeste' },
+  { id: 3, label: 'Sul' },
+  { id: 4, label: 'Sudeste' },
+];
+
+const ranking = [
+  { id: 1, label: 'Nacional' },
+  { id: 2, label: 'Internacional' },
+];
+
+export default function CarouselClub({items} : Images){      
     return (
-        <>
-        <div className={styles.container}>
+        <section className={styles.section}>
+          
+           <div className={styles.container}>
+           <div className={styles.dropdown}>
+              <Dropdown items={nacionalidade} label='Nacionalidade'></Dropdown>
+              <Dropdown items={modalidade} label='Modalidade'></Dropdown>
+              <Dropdown items={regiao} label='Região'></Dropdown>
+              <Dropdown items={ranking} label='Ranking'></Dropdown>
+              <button className={styles.button}>Aplicar</button>
+            </div>
+            
+            <div className={styles.slider}>
             <Swiper
-            speed={400}
-            spaceBetween={30}
-            slidesPerView={2}
-            slidesPerGroup={1}
-            centeredSlides={true}
-            loop={true}
-            breakpoints={{
-                560: {
-                slidesPerView: 2,
-                slidesPerGroup: 2
-                },
-                768: {
-                slidesPerView: 4
-                },
-                1024: {
-                slidesPerView: 4
-                },
-                1280: {
-                slidesPerView: 7
-                }
-            }}
+                breakpoints = {{
+                  1600:{
+                    slidesPerView: 7,
+                    slidesPerGroup: 1,
+                  },
+                  1450:{
+                      slidesPerView: 6,
+                      slidesPerGroup: 1,
+                  },
+                  1100:{
+                    slidesPerView: 5,
+                    slidesPerGroup: 1,
+                  },
+                  800: {
+                      slidesPerView: 4,
+                      slidesPerGroup: 1,
+                  },
+                  500: {
+                      slidesPerView: 3,
+                      slidesPerGroup: 1,
+                  }
+              }}
+                effect={'coverflow'}
+                centeredSlides={true}
+                loop={false}
+                coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 2.5,
+                slideShadows: false,
+                }}
+                navigation={{ enabled: true}}
+                modules={[EffectCoverflow, Navigation]}
+                className={styles.swiper_container}
             >
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-            <SwiperSlide>
-                <p className="bg-white p-4 rounded-lg shadow-md">Alo alo</p>
-            </SwiperSlide>
-
-            </Swiper>
-        </div>
-        </>
+                {items.map((image) => 
+                  <SwiperSlide key={image.key}>
+                     {({ isActive }) => (
+                      <div className={isActive ? styles.image_active : styles.image}>
+                          <Image src={image.src} width={image.width} height={image.height} alt={image.alt} ></Image>
+                      </div>   
+                    )}
+                 </SwiperSlide>   
+                )}
+                </Swiper>
+                </div>
+            </div>
+        </section>
       );
 }
--- src/modules/club/index.tsx
+++ src/modules/club/index.tsx
@@ -1,12 +1,86 @@
 import Header from "@/components/Header";
 import Footer from "../portal/components/footer";
-import Teste from './components/CarouselClub/teste'
+import CarouselClub from './components/CarouselClub'
 
 export default function ClubPage(){
+    const images = [
+        {
+          key: 1,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 2,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 3,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 4,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 5,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 6,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 7,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 8,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+        {
+          key: 9,
+          src: '/assets/images/union.svg',
+          caption: 'Brasão',
+          width: 200,
+          height: 215,
+          alt: 'Brasão',
+        },
+    ]
     return <>
         <div>
             <Header></Header>
-            <Teste></Teste>
+            <CarouselClub items={images}></CarouselClub>
             <Footer></Footer>
         </div>
     </>
