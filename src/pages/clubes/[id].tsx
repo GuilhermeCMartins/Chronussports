f3bdc5c06fa970fe58ab5eae669d404b7a902760
GuilhermeCMartins <guilhermecourbassier122@gmail.com>
2023-05-05 10:40:13 -0300

    feat: detailsClub

--- src/pages/clubes/[id].tsx
+++ src/pages/clubes/[id].tsx
@@ -8,6 +8,8 @@ interface Props {
   clube: Clube;
 }
 
+import { getAllClubes } from '@/utils/clubes';
+
 export default function ClubePage({ clube }: Props) {
   return <>
       <Header></Header>
@@ -18,18 +20,7 @@ export default function ClubePage({ clube }: Props) {
 
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const clubes: Clube[] = [
-    { id: 1 , nome: 'Sport',sufixo: 'Esporte Clube' , descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10},
-    { id: 2 , nome: 'Náutico',sufixo: 'Esporte Clube' , descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 3 , nome: 'Santa Cruz', sufixo: 'Esporte Clube', descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 4 , nome: 'Grêmio', sufixo: 'Esporte Clube' ,descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 5 , nome: 'Londrina', sufixo: 'Esporte Clube', descricao: 'O Londrina Esporte Clube é um clube de futebol localizado na cidade de Londrina, no norte do estado brasileiro do Paraná. É um dos principais clubes do estado, tendo sido fundado em 5 de abril de 1956.', endereco: 'Endereço do Clube A', src: '/assets/images/londrina.svg', valuation: 450.000, ranking: 36, internacional: null, points_cbf: 3700 },
-    { id: 6 , nome: 'Inter',sufixo: 'Esporte Clube' ,descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 7 , nome: 'Leiria',sufixo: 'Esporte Clube' ,descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 8 , nome: 'Litio',sufixo: 'Esporte Clube' ,descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 9 , nome: 'Episkopi',sufixo: 'Esporte Clube' ,descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-    { id: 10 , nome: 'Barcelona',sufixo: 'Esporte Clube' ,descricao: 'Descrição do Clube A', endereco: 'Endereço do Clube A', src: '/assets/images/brasao_example.svg', valuation: 450.000, ranking: 32, internacional: null, points_cbf: 10 },
-  ];
+  const clubes = getAllClubes();
 
   const paths = clubes.map((clube) => ({
     params: { id: clube.id.toString() },
